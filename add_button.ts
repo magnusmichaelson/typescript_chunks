d87c3916899d7bf65de675ac8d10e8a25c209d33
magnus <magnus@protonmail.com>
2020-12-01 18:52:20 +0000

    :)

--- add_button.ts
+++ add_button.ts
@@ -1,10 +1,30 @@
-    var parentDiv: HTMLElement;
-    var button: HTMLButtonElement;
-    // @ts-ignore
-    parentDiv = document.getElementById("top");
-    button = <HTMLButtonElement>document.createElement("BUTTON");
-    button.id = 'alpha';
-    button.type = 'button';
-    button.className = "foo";
-    button.innerHTML = 'alpha';
-    parentDiv.appendChild(button);
+
+interface ButtonElement {
+    id: string;
+    type: string;
+    className: string;
+    buttonText: string;
+    parentDiv: string;
+
+}
+
+    var button: HTMLButtonElement; 
+    button = createHtmlButton({"buttonText": "Csv", "className": "btn-primary", "id": "csv", "parentDiv": "top", "type": "button"});
+    button.addEventListener("click", dosomething, false);
+
+    function createHtmlButton(input: ButtonElement){
+        var button: HTMLButtonElement;
+        var parentDiv: HTMLElement;
+        // @ts-ignore
+        parentDiv = document.getElementById(input["parentDiv"]);
+        button = <HTMLButtonElement>document.createElement("BUTTON");
+        button.id = input["id"];
+        button.type = input["type"];
+        button.className = input["className"];
+        button.innerHTML = input["buttonText"];
+        parentDiv.appendChild(button);
+        return(button);
+    }
+
+    function dosomething(){
+    }
