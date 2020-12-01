53f88639b1a65447f4d67bcc38442b73417f2cd6
magnus michaelson <magnusmichaelson@protonmail.com>
2020-12-01 11:57:33 +0000

    :)

---
+++ add_space.ts
@@ -0,0 +1,6 @@
+    var parentDiv: HTMLElement;
+    var space: Text;
+    // @ts-ignore
+    parentDiv = document.getElementById("top");
+    space = document.createTextNode("\u00A0");
+    parentDiv.appendChild(space);
