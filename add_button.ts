f6ec3d563fa31806adfbb5132e517b831018fbb7
magnus michaelson <magnusmichaelson@protonmail.com>
2020-12-01 11:58:03 +0000

    :)

--- add_button.ts
+++ add_button.ts
@@ -1,6 +1,5 @@
     var parentDiv: HTMLElement;
     var button: HTMLButtonElement;
-    var space: Text;
     // @ts-ignore
     parentDiv = document.getElementById("top");
     button = <HTMLButtonElement>document.createElement("BUTTON");
@@ -9,7 +8,3 @@
     button.className = "foo";
     button.innerHTML = 'alpha';
     parentDiv.appendChild(button);
-    button.addEventListener("click", doSomething, false);
-    // space between buttons
-    space = document.createTextNode("\u00A0");
-    parentDiv.appendChild(space);
