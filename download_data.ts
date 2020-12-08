e6f3c3ce2094c8c17a5bd43e69c7711de64ff6d5
magnus michaelson <magnusmichaelson@protonmail.com>
2020-12-08 12:14:26 +0000

    :)

---
+++ download_data.ts
@@ -0,0 +1,16 @@
+var data = ['123','456'];
+download(data,'data.json');
+
+function download(data: any,filename: string){
+  var jsonData: any;
+  var encodedData: any;
+  var download: any;
+  jsonData = JSON.stringify(data, null, 2);
+  encodedData = 'data:text/plain;charset=utf-8,' + encodeURIComponent(jsonData);
+  download = document.createElement('a');
+  download.setAttribute('href', encodedData );
+  download.setAttribute('download', filename);
+  document.body.appendChild(download);
+  download.click();
+  document.body.removeChild(download);
+}
